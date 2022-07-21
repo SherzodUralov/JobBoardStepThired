2d04768b63a9f9c490e0539ffe5178bac9183a75
jurabek98.jalolov <jurabek98.jalolov@outlook.com>
2022-07-21 11:57:12 +0300

    Class lar yaratilyabdi...

---
+++ JobBoardStep.Data/Models/Region.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobBoardStep.Data.Models
+{
+    public class Region
+    {
+        public int RegionId { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+        public bool Status { get; set; }
+
+
+    }
+}
