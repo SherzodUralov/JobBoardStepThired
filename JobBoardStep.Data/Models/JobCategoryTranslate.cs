4cd4c257c427ddb1c4ccdbdea25dbbb2394debdc
Sherzod9808 <uralovsherzod03@gmail.com>
2022-07-21 14:15:20 +0500

    ikkta class qo'shildi

---
+++ JobBoardStep.Data/Models/JobCategoryTranslate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobBoardStep.Data.Models
+{
+    public class JobCategoryTranslate
+    {
+        public int JobCatTranslateId { get; set; }
+        public string? JobCatName { get; set; }
+
+    }
+}
