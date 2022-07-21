7e5694099f8d7f8b953241ff465886bc71e4c30c
jurabek98.jalolov <jurabek98.jalolov@outlook.com>
2022-07-21 14:53:38 +0300

    Contex Add

--- JobBoardStep/Controllers/HomeController.cs
+++ JobBoardStep/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-﻿using JobBoardStep.Models;
+﻿
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -23,10 +23,7 @@ namespace JobBoardStep.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        
+
     }
 }
